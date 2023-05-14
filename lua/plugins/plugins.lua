bbf201b485a84980098c032d8bc70005e7201321
mzusman <mor.zusmann@gmail.com>
2023-05-15 01:47:57 +0300

    init

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,11 @@
+return {
+
+  { "catppuccin/nvim", name = "catppuccin" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
