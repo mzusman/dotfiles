9c318d1ffc27e8b266dbfe4bb9a8c13dc76f1bc9
Mor Zusman <morz@ai21.com>
2024-07-21 11:13:24 +0300

    Rose pine2

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("mzus")
-vim.cmd.colorscheme "tokyonight-night"
+vim.cmd.colorscheme "rose-pine"
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
