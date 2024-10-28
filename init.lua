1ccfeffca3ccbaed2f1e58ab0de5c5d6ad783029
mzusman <mor.zusmann@gmail.com>
2024-10-28 11:36:06 +0200

    Changes

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("mzus")
-vim.cmd.colorscheme "rose-pine"
+vim.cmd.colorscheme "github_light"
+vim.cmd.colorscheme "github_light"
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
